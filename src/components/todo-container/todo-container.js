1a00a18d53b04cc9e26fa061d593e846008c20cb
myportfolios <lexybiade@gmail.com>
2021-02-05 20:33:00 -0500

    created folder structure and app ui

---
+++ src/components/todo-container/todo-container.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import TodoPresentation from "../todo-presentation/todo-presentation";
+
+export default class TodoContainer extends Component {
+  render() {
+    return (
+      <div>
+        <TodoPresentation />
+      </div>
+    );
+  }
+}
---
+++ src/components/todo-presentation/todo-presentation.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from "react";
+
+import "./todo.scss";
+
+export default function TodoPresentation() {
+  const emptyString = "";
+  const [checkbox, setCheckbox] = useState(true);
+  const [todoItem, setTodoItem] = useState(emptyString);
+  const [todoList, addTodoList] = useState([]);
+  const [checkedList, setCheckedList] = useState([]);
+
+  //clear userInput field
+  useEffect(() => {
+    setTodoItem(emptyString);
+  }, [todoList]);
+  //get user input
+  const handleUserInput = (e) => {
+    let code = e.which;
+    let key = e.key;
+    if (code === 13 || key === "Enter") {
+      addTodoHandler();
+    }
+    let value = e.target.value;
+    setTodoItem(value);
+  };
+
+  //save user input
+  const addTodoHandler = () => {
+    if (!todoItem) {
+      return;
+    }
+    addTodoList([...todoList, todoItem]);
+  };
+  //handle Checkbox
+  const handleCheckbox = (e, checked) => {
+    let duplicate = checkedList.includes(checked);
+    duplicate &&
+      setCheckedList(
+        checkedList.filter((item) => {
+          return item !== checked;
+        })
+      );
+    !duplicate && setCheckedList([...checkedList, checked]);
+
+    setCheckbox(true);
+    setCheckbox(!checkbox);
+  };
+  //list display
+  let listDisplay =
+    todoList &&
+    todoList.map((todo, index) => {
+      return (
+        <div
+          key={index}
+          className={checkedList.includes(todo) ? "is-done" : ""} //add style if checked
+        >
+          <li className="display-todo-box_todo">{todo}</li>
+          <input
+            type="checkbox"
+            className="display-todo-box__checkbox"
+            onClick={(e) => handleCheckbox(e, todo)}
+          />
+        </div>
+      );
+    });
+  let totalNoOfTodo = todoList && todoList.length; //total number of task
+  let remainingTask =
+    checkedList.length > 0 ? totalNoOfTodo - checkedList.length : totalNoOfTodo; //total number of outstanding task
+  return (
+    <div>
+      <h1>To do</h1>
+      <RenderInputWithBtn>
+        <div className="input-btn-box">
+          <input
+            type="text"
+            placeholder="Enter Todo"
+            className="input-btn-box__text-input"
+            onChange={handleUserInput}
+            value={todoItem}
+            onKeyPress={handleUserInput}
+          />
+          <input
+            type="submit"
+            className="input-btn-box__save-btn"
+            onClick={addTodoHandler}
+          />
+        </div>
+      </RenderInputWithBtn>
+      <RenderTodoList>
+        <ul>{listDisplay}</ul>
+      </RenderTodoList>
+      <h5>
+        {remainingTask} remaining out of {totalNoOfTodo}
+      </h5>
+    </div>
+  );
+}
+const RenderInputWithBtn = ({ children }) => {
+  return children;
+};
+
+const RenderTodoList = (props) => {
+  return props.children;
+};
